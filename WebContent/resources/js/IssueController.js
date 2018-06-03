5ec524507413c0a75f9a775ceb342e35c0f4e46f
kennedykas <kennedy0064@gmail.com>
2018-06-03 17:02:06 -0300

    Correção js de questões

--- WebContent/resources/js/IssueController.js
+++ WebContent/resources/js/IssueController.js
@@ -1,36 +1,102 @@
-;
-var unicid   = unicid || {} ;
-unicid.Issue = {};
 
-(function($, ns) {
+var Issue = {
 
-	"use strict";
+	$counter               : $('#counter'),
+	numberOfSelectedIssues : 0,
 
-	ns.version = "1.0";
+	init : function() {
 
-	ns.controller = {
+		this.addListeners();
+		this.fillTestDateAndTime();
+		this.cleanFieldsForNewTest();
+	},
 
-		init : function init() {
+	addListeners : function() {
 
-		 	this.addListeners();
-		},
-		
-		addListeners : function addListeners() {
-			
-			$('.check-answer').on('click', function(event) {
-				unicid.Issue.controller.checkIfSelectedAnswerHasText(event);
-			});
-		},
-		
-		checkIfSelectedAnswerHasText : function checkIfSelectedAnswerHasText(event) {
-			var selectedAnswerText = ($('#questao\\:' + "alternativa" + $(event.currentTarget).val()).val());
-			if(selectedAnswerText) {
-				$('#questao\\:' + "alternativaCorreta").val($(event.currentTarget).val());
-				return true;
-			} else {
-				M.toast({html: 'A alternativa selecionada está vazia!'});
-				event.preventDefault();				
-			}
+		$('.check-answer')           .on('click',  function(event){ Issue.checkAnswer(event); });
+		$('.filter-issues-btn')      .on('click',  function(){ Issue.filterIssuesByDiscipline(); });
+		$('.clear-filter-btn')       .on('click',  function(){ Issue.clearFilter();       });
+		$('.question-status')        .on('click',  function(){ Issue.updateCounter();     });
+		$('.save-test-btn')          .on('click',  function(){ Issue.saveTest();          });
+		$('.datepicker, .timepicker').on('change', function(){ Issue.updateDateAndTime(); });
+	},
+
+	checkAnswer : function(event) {
+
+		var selectedAnswer = $('#questao\\:' + 'alternativa' + event.currentTarget.value).val();
+
+		if (selectedAnswer) {
+			$('#questao\\:' + "alternativaCorreta").val(event.currentTarget.value);
+			return true;
+		} else {
+			M.toast({html: 'A alternativa selecionada está vazia.'});
+			event.preventDefault();
 		}
-	};
-})(jQuery, unicid.Issue);
+	},
+
+	filterIssuesByDiscipline : function() {
+
+		var $issues             = $('.issues-container');
+		var selectedDisciplines = $('#disciplines').val();
+
+		if(selectedDisciplines.length) {
+
+			$issues.closest('td').hide();
+
+			for (let i in selectedDisciplines)
+
+				$('.' + selectedDisciplines[i]).closest('td').show();
+		}
+	},
+
+	clearFilter : function() {
+
+		$('.issues-container').closest('td').show();
+
+		M.toast({html: 'Mostrando todas as questões'});
+	},
+
+	updateCounter : function() {
+
+		this.numberOfSelectedIssues = $('.question-status input[type=checkbox]:checked').length;
+
+		if (this.numberOfSelectedIssues)
+			this.$counter.html(this.numberOfSelectedIssues).removeClass('scale-out').addClass('scale-in');
+		else
+			this.$counter.toggleClass('scale-out scale-in');
+	},
+
+	saveTest : function() {
+
+		if (this.numberOfSelectedIssues) {
+			$('#test-info\\:title').focus();
+			$('#test-info').removeClass('scale-out').addClass('scale-in');
+		} else {
+			M.toast({html: 'Selecione pelo menos uma questão.'});
+			$('#test-info').toggleClass('scale-out scale-in');
+		}
+	},
+
+	updateDateAndTime : function() {
+
+		$("#test-info\\:dateAndTime").val(
+			$('.datepicker').val() + "-" + $('.timepicker').val());
+	},
+
+	fillTestDateAndTime : function() {
+
+		if ($("#test-info\\:dateAndTime").val() !== '') {
+
+			let dateAndTime = $("#test-info\\:dateAndTime").val().split("-");
+
+			$('.datepicker').val(dateAndTime[0]);
+			$('.timepicker').val(dateAndTime[1]);
+		}
+	},
+
+	cleanFieldsForNewTest : function() {
+
+		if (this.$counter.length)
+			$('#test-info\\:value, #test-info\\:time').val(null);
+	}
+};
--- WebContent/resources/js/util.js
+++ WebContent/resources/js/util.js
@@ -1,12 +1,12 @@
-//--------------------------------- 
+//---------------------------------
 //           DOM READY
 //---------------------------------
 $(document).ready(function(){
-	init();	
+	init();
 });
 
 
-//   AFTER CONTENT LOAD RECALL INIT FOR MATERIALIZE COMPONENTS 
+//   AFTER CONTENT LOAD RECALL INIT FOR MATERIALIZE COMPONENTS
 //---------------------------------------------------------------
 function loadContent(content) {
 	$(".mainContent").load(content,function() {
@@ -15,34 +15,33 @@ function loadContent(content) {
 }
 
 function init(){
-	
+
 	$('.sidenav')   .sidenav();
-	$('select')     .formSelect();        
+	$('select')     .formSelect();
 	$('.modal')     .modal();
-	$('.tooltipped').tooltip();
-	 
+
 	$('.loaderContainer').hide();
-	
+
 	$('.number') .val(null); // the null here is to prevent the default value 0
 
 	$('.cpf')    .mask('999.999.999-99');
 	$('.date')   .mask('99/99/9999-99:99',{placeholder:'DD/MM/AAAA-HH:MM'});
 	$('.celular').mask('(99) 99999-9999');
 	$('.money').on( "keyup", function( event ) {
-            
+
 		// When user select text in the document, also abort.
 		var selection = window.getSelection().toString();
 		if ( selection !== '' )
 			return;
-		
+
 		// When the arrow keys are pressed, abort.
 		if ( $.inArray( event.keyCode, [38,40,37,39] ) !== -1 )
 			return;
-	
+
 		var $this = $( this );
-		
+
 		var input = $this.val();
-		
+
 		var input = input.replace(/[\D\s\._\-]+/g, "");
 		input = input ? parseInt( input, 10 ) : 0;
 
@@ -50,25 +49,25 @@ function init(){
 			return ( input === 0 ) ? "" : input.toLocaleString( "pt-BR" );
 		});
 	});
-		
+
 	// facemessage links (making they work)
 	$(".esc").each(function(i) {
-		var h = $(this).html();                
+		var h = $(this).html();
 		h = h.replace(/&lt;/gi, "<");
 		h = h.replace(/&gt;/gi, ">");
 		$(this).html(h);
 	});
-	
+
 	// email field (.emailField)
 	var emailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 	$('.emailField').on('change', function() {
 		if (emailRegex.test($(this).val())) {
 			$(this).addClass('valid').removeClass('invalid');
 			$('button.validate').prop("disabled", false);
-		} else 
+		} else
 			$(this).addClass('invalid').removeClass('valid');
 	});
-	
+
 	// DATE PICKER
 	var options = {
 		format   : 'dd/mm/yyyy',
@@ -103,7 +102,7 @@ function init(){
 				'Nov',
 				'Dez'
 			],
-			'weekdaysShort' : [						
+			'weekdaysShort' : [
 				'Dom',
 				'Seg',
 				'Ter',
@@ -115,37 +114,38 @@ function init(){
 			'weekdaysAbbrev' : ['D','S','T','Q','Q','S','S']
 		}
 	};
-	var elem = document.querySelector('.datepicker');
-	var instance = M.Datepicker.init(elem, options);
-	
+	var $dateElems = $('.datepicker');
+	M.Datepicker.init($dateElems, options);
+
+
 	// TIME PICKER
-	$('.timepicker').timepicker({
-	    default      : 'now',
-	    fromnow      : 0,         // set default time to * milliseconds from now (using with default = 'now')
-	    twelvehour   : false,     // Use AM/PM or 24-hour format
-	    donetext     : 'OK',
-	    cleartext    : 'Limpar',
-	    canceltext   : 'Cancelar',
-	    autoclose    : true,
-	    ampmclickable: true
-	});
-	
+	var timerOptions = { 'twelveHour' : false };
+	var $timerElems = $('.timepicker');
+	M.Timepicker.init($timerElems, timerOptions);
+
+
 	// SET THE CURRENT PAGE AS SELECTED ON NAVBAR
 	$('.navItem').find('.active').removeClass('active');
-	
+
 	var activeWindow = $('.mainContent').data('content');
 
 	$('.navItem').each(function(){
-		
+
 		if ($(this).data('page') == activeWindow)
 			$(this).addClass('active');
 	});
-	
+
 	// SUCCESS MESSAGES
 	var toastMessage = new URL(window.location.href).searchParams.get("toast");
 	M.toast({html: toastMessage});
-	 
+
 	M.updateTextFields();
+
+	M.ScrollSpy.init($('.scrollspy'));
+
+	M.FloatingActionButton.init($('.fixed-action-btn'), {'direction' : 'left'});
+
+    M.Tooltip.init($('.tooltipped'));
 }
 
 //        SHOW PAGE LOADING ANIMATION
@@ -155,33 +155,10 @@ $(".loading").on("submit", function() {
 });
 
 
-//          UPDATE QUESTIONS COUNTER
-//
-//	@usedIn listaQuestoes.xhtml
-//---------------------------------------------
-function updateCounter() {
-	
-	// get all selected questions
-	var numberOfSelectedQuestions = 
-		document.getElementsByClassName('bordered')[0]
-			.querySelectorAll('input[type=checkbox]:checked').length;
-	
-	// get the counter element
-	var counter = document.getElementById('selectedQuestionsCounter');
-	
-	counter.innerHTML = numberOfSelectedQuestions;
-	
-	if(numberOfSelectedQuestions > 0)
-		counter.style.display = 'block';
-	else
-		counter.style.display = 'none';
-	
-}
-
 /**
  * Valida CPF
- * 
- */ 
+ *
+ */
 $(".cpf").on("keyup", function(){
 	// EXTRAI APENAS OS NUMEROS
 	cpf = $(".cpf").val().match(/\d/g).join("");
@@ -192,23 +169,23 @@ $(".cpf").on("keyup", function(){
     	return false;
     }
     // VALIDA O PRIMEIRO DIGITO
-    add = 0;    
-    for (i=0; i < 9; i ++)       
-        add += parseInt(cpf.charAt(i)) * (10 - i);  
-        rev = 11 - (add % 11);  
-        if (rev == 10 || rev == 11)     
-            rev = 0;    
+    add = 0;
+    for (i=0; i < 9; i ++)
+        add += parseInt(cpf.charAt(i)) * (10 - i);
+        rev = 11 - (add % 11);
+        if (rev == 10 || rev == 11)
+            rev = 0;
         if (rev != parseInt(cpf.charAt(9))) {
         	$('.cpf').addClass('invalid');
         	return false;
-        }     
+        }
     // VALIDA O SEGUNDO DIGITO
-    add = 0;    
-    for (i = 0; i < 10; i ++)        
-        add += parseInt(cpf.charAt(i)) * (11 - i);  
-    rev = 11 - (add % 11);  
-    if (rev == 10 || rev == 11) 
-        rev = 0;    
+    add = 0;
+    for (i = 0; i < 10; i ++)
+        add += parseInt(cpf.charAt(i)) * (11 - i);
+    rev = 11 - (add % 11);
+    if (rev == 10 || rev == 11)
+        rev = 0;
     if (rev != parseInt(cpf.charAt(10))) {
     	$('.cpf').addClass('invalid');
     	return false;
@@ -217,52 +194,6 @@ $(".cpf").on("keyup", function(){
     $('.cpf').removeClass('invalid').addClass('valid');
     return true;
 });
-
-
-/**
- * Set the selected date and time on the hidden input
- */
-var data;
-var horario;
-
-$('#date').on('change', function() {
-	
-	var dateAux = $(this).val().replace(" ", " ").replace(", ", " ").split(" ");
-	
-	data = dateAux[0] + "/" + getMonthFromString(dateAux[1]) + "/" +dateAux[2];
-	
-	if(horario != undefined)
-		$("#testInfo\\:dateAndTime").val(data + "-" + horario);
-});
-
-$('#schedule').on('change', function() {
-	
-	horario = $(this).val();
-	
-	if(data != undefined)
-		$("#testInfo\\:dateAndTime").val(data + "-" + horario);
-});
-
-function fillDateAndTime() {
-	
-	var dateAndTime = $("#testInfo\\:dateAndTime").val().split("-");
-	
-	data = dateAndTime[0];
-	horario = dateAndTime[1];
-	
-	$("#date").val(dateAndTime[0]);
-	$("#schedule").val(dateAndTime[1]);
-}
-
-function getMonthFromString(mon){
-
-   var d = Date.parse(mon + "1, 2018");
-   
-   if(!isNaN(d))
-      return new Date(d).getMonth() + 1;
-   
-   return -1;
-}
 
 
 //     CHECA SE PASSWORDS CORRESPONDEM NOS DOIS CAMPOS (newUsers)
@@ -281,7 +212,7 @@ $(".password").on("keyup", function(){
 //         MOSTRAR CAMPO INTEGRANTE (novoGrupo.xhtml)
 //--------------------------------------------------------------
 $(".showNextMemberField").on("click", function(){
-	
+
 	// make the input take all available space
 	$("." + $(this).data("next") + " > input").removeClass('s10');
 
@@ -292,16 +223,6 @@ $(".showNextMemberField").on("click", function(){
 	$("." + $(this).data("next")).show();
 });
 
-//     CHECKING IF AT LEAST ONE ALTERNATIVE WAS SELECTED (listaQuestoes.xhtml)
-//---------------------------------------------------------------------------------
-function checkQuestions() {
-	if($('#questionList input[type=checkbox]:checked').length) 
-		return true;
-	else {
-		alert("É necessário selecionar pelo menos uma questão para a sua prova!");
-		return false;
-	}
-}
 
 //             TIMER (listaProvaAluno.xhtml)
 //---------------------------------------------------------
@@ -309,23 +230,23 @@ var seconds = 0;
 var minutes = 0;
 var minutes = $("#showProva\\:minutes").val();
 $("#showProva\\:countdown").text(minutes);
-function display() { 
-	if (seconds <= 0) { 
-		seconds = 60; 
-		minutes -= 1; 
-	} 
-	if (minutes <= -1) { 
-		seconds = 0; 
+function display() {
+	if (seconds <= 0) {
+		seconds = 60;
+		minutes -= 1;
+	}
+	if (minutes <= -1) {
+		seconds = 0;
 		minutes += 1;
 		alert("Seu tempo acabou as suas respostas foram salvas!")
 		window.location.href="gabaritoAluno.xhtml";
-	} 
-	else 
+	}
+	else
 		seconds -= 1;
 	$("#showProva\\:countdown").text(minutes + ":" + seconds);
-	setTimeout("display()",1000); 
-} 
-display(); 
+	setTimeout("display()",1000);
+}
+display();
 
 //        PAGE AUTO RELOAD (listaProvasAluno.xhtml)
 //---------------------------------------------------------
@@ -334,49 +255,11 @@ setInterval(function() {
 	}, 55000
 );
 
-//        FILTRO DISCIPLINAS (listaQuestoes.xhtml)
-//----------------------------------------------------------------------
-$(".filterQuestionsByDiscipline").on('click', function(){
-
-	var questions = document.getElementsByClassName('questionContainer');
-	var disciplinesAvailableToChoose = 
-		document.getElementById('disciplines').getElementsByTagName('input'); 
-	
-	if(disciplinesAvailableToChoose.length) {
-
-		for(var j = 0; j < questions.length; j++) {
-
-			$(questions[j]).closest('td').css('display', 'none');
-			
-			for(var k = 0; k < disciplinesAvailableToChoose.length; k++) {
-				
-				if(disciplinesAvailableToChoose[k].checked && 
-						questions[j].classList.contains(disciplinesAvailableToChoose[k].value))
-	
-					$(questions[j]).closest('td').css('display', 'table-cell');
-			}
-		}
-	}
-});
-
-$('.clearFilter').on('click', function(){
-	
-	var questions = document.getElementsByClassName('questionContainer')
-	var disciplinesAvailableToChoose = 
-		document.getElementById('disciplines').getElementsByTagName('input') 
-	
-	for (var i = 0; i < questions.length; i++)  
-		$(questions[i]).closest('td').css('display', 'table-cell')
-	
-	for (var i = 0; i < disciplinesAvailableToChoose.length; i++)
-		disciplinesAvailableToChoose[i].checked = false
-});
-
 //               FILTRO CURSOS (listaDisciplinas.xhtml)               MANUTENCAO
 //----------------------------------------------------------------------
 $(".filtrarCurso").on("click", function(){
 	var cursoSelecionado = $("select option:selected").text(); // OBTENHO CURSO SELECIONADO
-	$("tbody tr").hide(300); // ESCONDO TODOS OS CURSOS	
+	$("tbody tr").hide(300); // ESCONDO TODOS OS CURSOS
 	$("td").each(function() {
 	    if ($(this).text().indexOf(cursoSelecionado) >= 0)  // MOSTRO APENAS AS LINHAS QUE CONTEM O CURSO SELECIONADO
 	    	$(this).parents("tr").show(300);
